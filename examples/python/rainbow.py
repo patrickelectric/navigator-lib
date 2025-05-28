08440a852fb2c6b04463526076783643c5e2a468
Patrick José Pereira <patrickelectric@gmail.com>
2025-05-28 16:50:08 -0300

    examples: python: rainbow: Add test for aarch64 to set pi and navigator version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/python/rainbow.py
+++ examples/python/rainbow.py
@@ -18,6 +18,14 @@ def color_from_sine(percentage):
 
 
 def main():
+    if platform.machine() == "aarch64":
+        # It's possible to set the configuration before initializing the navigator, check this example
+        from bluerobotics_navigator import NavigatorVersion, Raspberry
+        print("Setting up for Navigator V2 on Raspberry Pi 5")
+        navigator.set_rgb_led_strip_size(1)
+        navigator.set_navigator_version(NavigatorVersion.Version2)
+        navigator.set_raspberry_pi_version(Raspberry.Pi5)
+
     navigator.init()
 
     print("Creating rainbow effect!")
